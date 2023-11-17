2a9ce6d1e6386c85bb4f34539ffb0421fd8dee33
moohamedmagdi <moohamedmagdi@gmail.com>
2023-11-17 00:05:00 +0000

    hi there

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,14 +3,15 @@
 starts a Flask web application
 """
 
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
+def hello():
     """function to return Hello HBNB!"""
-    return 'Hello HBNB!'
+    return render_template("10-hbnb_filter.html")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port='5000', debug=None)
